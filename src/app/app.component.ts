827ad1f33833e23918c9c78f200be6158e976572
GaryLiao <Gary.Liao@ibm.com>
2021-07-17 18:57:20 +0800

    Used odd and even component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gameApp';
+  oddComponents: number[] = [];
+  evenComponents: number[] = [];
+
+  handleCurrentSecond(poppedCurrentSecond: number): void {
+    console.log(poppedCurrentSecond);
+    if(this.isEven(poppedCurrentSecond)){
+      this.evenComponents.push(poppedCurrentSecond);
+    }
+    if(this.isOdd(poppedCurrentSecond)){
+      this.oddComponents.push(poppedCurrentSecond);
+    }
+  }
+
+  isEven(num: number): boolean {
+    return num % 2 === 0;
+  }
+
+  isOdd(num: number): boolean {
+    return num % 2 !== 0;
+  }
 }
